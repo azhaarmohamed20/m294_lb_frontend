674f0d47ae2bdf682c6ba402244ed2c623c92531
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-02-02 08:38:39 +0100

    Erstelle Interface für Eingabe-Props

---
+++ src/components/Inputfield.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export interface Inputprops{
+    input: String;
+}
+
+
+const InputField: React.FC<Inputprops> = (input) =>{
+return(
+    <div>
+
+    </div>
+)
+
+}
+
+export default InputField;
