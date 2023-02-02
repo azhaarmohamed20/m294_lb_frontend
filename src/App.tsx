a0ace98bd6eae4f6c953ba43ac8fd740cf3f5349
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-02-02 08:52:41 +0100

    Komponenten und UseState eingefügt

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import HandleToDo from './components/HandleToDo';
+import InputField from './components/Inputfield';
 
 function App() {
+  const [inputs, SetInputs]=useState("");
+
   return (
     <div className="App">
-      
+      <InputField input={inputs}/>
+      <HandleToDo inputValue={inputs}/>
     </div>
   );
 }
