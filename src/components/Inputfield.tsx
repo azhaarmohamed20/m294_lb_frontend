18d40fb211faf155894b8e1cdfffff7c6880d7c7
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-02-02 09:43:10 +0100

    OnChange und OnClick Funktionen implementiert

--- src/components/Inputfield.tsx
+++ src/components/Inputfield.tsx
@@ -1,18 +1,35 @@
-import React from "react";
-import { text } from "stream/consumers";
+import React, { useState } from "react";
+
 
 export interface Inputprops{
-    input: String;
+    input: String,
 }
 
 
 const InputField: React.FC<Inputprops> = (input) =>{
-return(
-    <div>
-        <input type="text" placeholder="Add a Task"></input>
-        <input type="submit"value="Add"></input>
-    </div>
-)
+    // UseState für Eingabe
+    const [eingabe, SetEingabe]=useState("");
+    // OnChange um eingabe zu verarbeiten
+    const OnChange = (event: { target: { value: React.SetStateAction<string>; }; }) =>{
+        SetEingabe(event.target.value);
+    }
+
+    // OnClick um ein Event beim Mouse Klick zu triggern
+    const Click = (e: React.MouseEvent<HTMLInputElement>)=>{
+        // Eingabe in Konsole machen
+        console.log(eingabe);
+        // Feld Leeren
+        SetEingabe("");
+    }
+
+    return(
+        <div>
+            {/*input Feld für Text. Eingabe als value und mit OnChange funktion */}
+            <input type="text" id="task" placeholder="Add a Task"value={eingabe} onChange={OnChange}></input>
+            {/*input Feld für Submit. Mit OnClick funktion */}
+            <input type="submit"value="Add" onClick={Click}></input>
+        </div>
+    )
 
 }
 
