744dc181318a53b1096a0861411f819c3f54a11e
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-02-02 13:03:58 +0100

    Get und Delete Request eingefügt.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import HandleToDo from './components/HandleToDo';
 import InputField from './components/Inputfield';
 
+export type Task = {
+  id: number,
+  title: String,
+  complete: boolean;
+}
+
 function App() {
   const [inputs, SetInputs]=useState("");
+
+  const[task, SetTasks] = useState<Task[]>([]);
+
+    useEffect(() =>{
+        loaddata();
+    },[]);
+
+    function loaddata (){
+        axios.get<Task[]>("http://localhost:3001/tasks").then((response)=>{
+            SetTasks(response.data);
+        });
+    }
+
+    function deleteTask(tasktodelete: Task){
+        axios.delete("http://localhost:3001/task/" + tasktodelete.id ).then (() =>{
+            loaddata();
+        })
+    }
+
+
 
   return (
     <div className="App">
