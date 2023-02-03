5aaf8616cb28a0e6bf22ec295fd3c4afe4a83fbd
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-02-03 08:55:47 +0100

    Design für ToDoList implementiert

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -9,11 +9,12 @@ export interface HandleProps{
 
 function TodoList(props: HandleProps){
     return(
+        <form className="form">
         <div id="lielement">
             {props.tasks.map((todo:Task)=>(
-            <li key={todo.id}>{todo.title}<button onClick={()=> props.deleteTask(todo)} className="libutton">delete</button><button onClick={() => props.editTask(todo)} className="libutton">Edit</button></li>
+            <li key={todo.id} >{todo.title}<button onClick={()=> props.deleteTask(todo)} className="libutton">delete</button><button onClick={() => props.editTask(todo)} className="libutton">Edit</button></li>
             ))}
-        </div>
+        </div></form>
     )
 }
 
